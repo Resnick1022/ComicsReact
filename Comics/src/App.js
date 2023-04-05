df99c66aa8e0c804b99da0a7ca4db40356a4e0a3
Resnick1022 <114195739+Resnick1022@users.noreply.github.com>
2023-04-04 20:29:03 -0400

    Add files via upload

---
+++ Comics/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+import UserInterface from "./UserInterface"
+import StylesContext from "./StylesContext";
+
+
+function App() {
+
+  const stylin = {
+    background: "#1ECD97",
+    display: "inline-block",
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15,
+    cursor: "pointer"             
+  }
+
+  return (
+    <main>
+      <StylesContext.Provider value={stylin}>
+        <h1
+            style={{
+              color: "#FEE001",
+              fontFamily: "Bangers",
+              fontSize: 45
+            }}
+          >
+          Welcome to Midtown Comics!
+        </h1>
+        <UserInterface />
+      </StylesContext.Provider>
+    </main>
+  );
+}
+
+export default App;
